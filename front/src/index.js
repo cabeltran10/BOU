59561c97d4860a67bfc2ca38731651276216eb3d
ar.corinaldi <ar.corinaldi@uniandes.edu.co>
2020-04-07 15:35:13 -0500

    navbar temporal

--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Navbar from "./navbar";
 
 function App(props) {
   return (
     <div>
+      <Navbar></Navbar>
       Hello {props.name}! 
     </div>
   );

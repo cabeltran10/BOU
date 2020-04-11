f13c1c87933ce24ef2f09cde61b1f1e0ffda0500
Allan Corinaldi <ar.corinaldi@uniandes.edu.co>
2020-04-11 11:33:36 -0500

    modularizing things

--- front/src/ListStore.js
+++ front/src/ListStore.js
@@ -1,41 +1,84 @@
-import React, {Component} from 'react';
+import React, { useState } from "react";
+import { Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
-class ListStore extends Component {
-  
-render()
-{
+import ShoppingCart from "./ShoppingCart";
+
+const products = [
+  {
+    id: "91823",
+    name: "Karen Isgrigg",
+    price: "400",
+    imageURL: "http://localhost:5001/karen.jpg",
+    quantity: 1,
+  },
+  {
+    id: "238470",
+    name: "Richard Kalehoff",
+    price: "400",
+    imageURL: "http://localhost:5001/richard.jpg",
+    quantity: 1,
+  },
+  {
+    id: "2037492",
+    name: "Vestido negro",
+    price: "400",
+    imageURL: "http://localhost:5001/tyler.jpg",
+    quantity: 1,
+  },
+];
+
+function ListStore(props) {
+  const [productsnew, setproducts] = useState(products);
+
+  function quantitylist(list) {
+    const newlist = {};
+    list.forEach(
+      (p) =>
+        (newlist[p] = {
+          name: p.name,
+          price: p.price,
+          id: p.id,
+          imageURL: p.imageURL,
+          quantity: 0,
+        })
+    );
+    return newlist;
+  }
+
+  const remove = (itemId) => {
+    const list = productsnew.filter((item) => item.id !== itemId);
+    setproducts(list);
+  };
   return (
-    <div>
-  <h1>Selecciona la tienda</h1>
-  <ul className='shop-list'>
+    <Router>
+      <div>
+        <h1>Selecciona la tienda</h1>
+        <ul className="shop-list">
+          {props.shops.map((shop) => (
+            <li key={shop.id} className="card shop-list-item">
+              <img className="card-img-top shop-avatar" src={shop.imageURL} />
 
-  {this.props.shops.map(shop => (
-
-
-    <li key={shop.id} className='card shop-list-item'>
-
-    <img className='card-img-top shop-avatar' src={shop.imageURL}/>
-
-
-    <div className='card-body shop-details'>
-
-    <h3 className='card-title'>{shop.name} </h3>
-   
-
-
-    <button className='shop-go'>
-    Comprar
-    </button>
-
-    </div>
-     </li>
-    ))}
-
-  </ul>
-
-    </div>
+              <div className="card-body shop-details">
+                <h3 className="card-title">{shop.name} </h3>
+                <Switch>
+                  <Route
+                    exact
+                    path={"/" + shop.name}
+                    component={() => (
+                      <ShoppingCart products={productsnew} onDelete={remove} />
+                    )}
+                  ></Route>
+                </Switch>
+                <Link to={"/" + shop.name}>
+                  <button className="shop-go">Comprar</button>
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Router>
   );
-}
 }
 
 export default ListStore;
--- front/src/ShoppingCart.js
+++ front/src/ShoppingCart.js
@@ -1,85 +1,75 @@
+import React, { useState } from "react";
+import AddToCart from "./AddToCart";
 
-import React, {Component, useState} from 'react';
+const item = {
+  id: "91823",
+  name: "Karen Isgrigg",
+  price: "400",
+  imageURL: "http://localhost:5001/karen.jpg",
+  description: "Ropitalindanuevapahueler",
+};
 
+function ShoppingCart(props) {
+  let gettotal = () => {
+    props.products.forEach((p) => total + p.price);
+  };
 
+  const [total, setTotal] = React.useState(0);
+  const [quantity, setQuantity] = React.useState(1);
 
-function ShoppingCart(props){
+  let onClickTotal = (item) => {
+    setTotal(total + item.price * item.quantity);
+    setQuantity(item.quantity + 1);
+  };
 
+  let onClickremove = (item) => {
+    setTotal(total - item.price * item.quantity);
+    setQuantity((item.quantity = 0));
+    props.onDelete(item.id);
+  };
 
-let gettotal =()=>
-{   
-    props.products.forEach(p=> total+p.price);
-}
+  return (
+    <div>
+      <h1>Resumen de productos</h1>
 
-const [total, setTotal]=React.useState(0);
-const [quantity, setQuantity]=React.useState(1);
+      <div className="card product-list">
+        {props.products.map((item) => (
+          <div className="container">
+            <div key={item.id} className="product-list-item">
+              <div
+                className="item-image"
+                style={{ backgroundImage: `url(${item.imageURL})` }}
+              ></div>
 
+              <div className="item-details">
+                <p className="item-name">{item.name}</p>
+              </div>
 
-let onClickTotal =(item)=>
-{
- setTotal(total+item.price*item.quantity);
- setQuantity(item.quantity+1);
+              <button onClick={() => onClickTotal(item)} className="item-add">
+                +
+              </button>
+              <span>{item.quantity}</span>
 
-}
+              <button
+                onClick={() => onClickremove(item)}
+                className="item-remove"
+              >
+                Remove
+              </button>
 
-let onClickremove =(item)=>
-{
- setTotal(total-item.price*item.quantity);
- setQuantity(item.quantity=0);
- props.onDelete(item.id);
-
-}
-
-    
-    return ( 
-	
-      <div>
-        <h1>Resumen de productos</h1>
-       
-
-        <div className='card product-list'>
-    
-        { props.products.map(item => (
-    
-        <div className='container'>
-            <div key={item.id} className='product-list-item' >
-            <div className='item-image' style={ {backgroundImage: `url(${item.imageURL})`}}>
+              <p>${item.price}</p>
             </div>
+          </div>
+        ))}
+      </div>
 
-            < div className='item-details'>
-
-            <p className='item-name'>{item.name}</p>
-     
-            </div>
-
-           <button onClick={ ( )=> onClickTotal(item) }  className='item-add'>
-            +
-            </button>
-            <span>{item.quantity}</span>
-
-
-            
-            <button onClick={( )=> onClickremove(item)} className='item-remove'>
-            Remove
-            </button>
-
-                   <p>${item.price}</p>
-            
-             </div>
-             </div>
-            ))}
-    
-        </div>
-
-
-        <div className='total' >
+      <div className="total">
         <h2>Total</h2>
         <p>${total}</p>
-        </div>
-          </div>
+      </div>
+      <AddToCart item={item} />
+    </div>
+  );
+}
 
-    
-    )
-        }
-
-export default ShoppingCart
+export default ShoppingCart;
